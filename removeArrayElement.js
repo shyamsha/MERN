c4e549b18c4d04040922f2f7bccaf5664a595e92
sHyaM <shyamkumar97@live.com>
2019-02-06 18:09:28 +0530

    Create removeArrayElement.js
    Write a JavaScript function to remove a specific element from an array. 
    
    Test data :
    console.log(removeArrayElement([2, 5, 9, 6], 5));
    output - [2, 9, 6]
    console.log(removeArrayElement([2, 5, 9,'g', 6], 'g'));
    output - [2,5, 9, 6]

---
+++ removeArrayElement.js
@@ -0,0 +1,11 @@
+function removeArrayElement(array,value){
+    
+    let index = array.indexOf(value)
+    if (index === -1) {
+            return 'invalid input'
+        }else{
+         array.splice(index, 1)
+        }
+return array
+}
+console.log(remove([2,3,5,'g',65,6],'g'))
