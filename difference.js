9d505e949899b657bd506114b5cdcc48bdc47e52
sHyaM <shyamkumar97@live.com>
2019-02-11 17:38:28 +0530

    Create difference.js
    Write a JavaScript function to find the unique elements from two arrays. 
    
    Test Data :
    1. difference([1, 2, 3], [100, 2, 1, 10]);
    return ["1", "2", "3", "10", "100"]
    
    2. difference([1, 2, 3], [100, 2, 1, 10]); 
    return ["1", "2", "3", "10", "100"]

---
+++ difference.js
@@ -0,0 +1,13 @@
+function difference(firstArray,secondArray){
+    let add = [...firstArray,...secondArray]  //spread operator
+    
+    if (firstArray.length == 0 && secondArray.length == 0) {
+        return 'empty array'
+    } 
+    let unique = add.filter(function (value,index) { 
+        return add.indexOf(value)===index
+    })
+     return unique
+    }
+   
+
