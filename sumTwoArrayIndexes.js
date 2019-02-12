8b93a0eddb0c69c497ca67188eeb967b7ff17a7c
sHyaM <shyamkumar97@live.com>
2019-02-12 20:50:41 +0530

    Create sumTwoArrayIndexes.js
    There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
    
    Sample array : 
    array1 = [1,0,2,3,4];
    array2 = [3,5,6,7,8,13];
    Expected Output : 
    [4, 5, 8, 10, 12, 13]

---
+++ sumTwoArrayIndexes.js
@@ -0,0 +1,16 @@
+function sumTwoArrayIndexes(array1,array2){
+    sum = []
+    for (let i = 0; i < array1.length; i++){
+        for (let j = 0; j < array2.length; j++) {
+            if (array1.length > array2.length) {
+                array2.push(0)
+            } else if (array2.length > array1.length) {
+                array1.push(0)
+            }
+            if (i==j) { 
+                sum.push(array1[i]+array2[i])
+            }
+        }
+    }
+return sum
+}
