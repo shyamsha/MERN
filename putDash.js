f15db684f401ce0e54969cb2927afc2c589ca493
sHyaM <shyamkumar97@live.com>
2019-02-18 22:45:47 +0530

    Create putDash.js
    Title: Dash between even
    
    Body:
    
    Write a JavaScript function which accepts a number as input and inserts dashes (-) between every two even numbers. For example if you accept putDash('025468') the output should be '0-254-6-8'.

---
+++ putDash.js
@@ -0,0 +1,13 @@
+function putDash(string){
+    let each = string.split('')
+    let even = ''
+    for (var i = 0; i < each.length; i++){
+        if (each[i]%2==0&&each[i+1]%2==0) {
+            even+=each[i] + '-'
+        } else {
+            even+=each[i]
+        }
+    
+    }
+return even
+}
