1f2163b9bb6c753bf9b4f451ac671818e3023bf2
sHyaM <shyamkumar97@live.com>
2019-03-23 19:59:17 +0530

    Add files via upload

---
+++ sumOfSqure.js
@@ -0,0 +1,8 @@
+function squareSum(a) {
+    var sum = 0;
+    for (var i = 0; i < a.length; i++) {
+        sum += a[i] * a[i];
+    }
+    return sum;
+}
+console.log(squareSum([1, 2, 3, 4, 5, 6, 7, 8, 9]));
